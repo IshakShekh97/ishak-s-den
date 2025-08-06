ea2ffc88493661d3ae3b23fad67fcb4eaf910c2b
Ishak Shekh <shekhishak90@gmail.com>
2025-08-06 17:34:04 +0530

    Remove Sanity-related files and configurations to streamline the project structure and eliminate unused code. This includes the deletion of the studio page, environment variables, client setup, image handling, live content API, schema definitions, and type generation files.

--- src/sanity/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-import { type SchemaTypeDefinition } from "sanity";
-import { portfolioType } from "./portfolioSchema";
-import { projectSchema } from "./projectSchema";
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [portfolioType, projectSchema],
-};
